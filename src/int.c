fe2bd03936c0d0598af369c9a695b60410537870
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-07-05 14:01:26 +0200

    Fix asserts

--- src/int.c
+++ src/int.c
@@ -6,21 +6,21 @@
 
 int
 compact_preencode_int (compact_state_t *state, intmax_t n) {
-  assert(sizeof(intmax_t) == 64);
+  assert(sizeof(intmax_t) == 8);
 
   return compact_preencode_uint(state, compact_encode_zig_zag(64, n));
 }
 
 int
 compact_encode_int (compact_state_t *state, intmax_t n) {
-  assert(sizeof(intmax_t) == 64);
+  assert(sizeof(intmax_t) == 8);
 
   return compact_encode_uint(state, compact_encode_zig_zag(64, n));
 }
 
 int
 compact_decode_int (compact_state_t *state, intmax_t *result) {
-  assert(sizeof(intmax_t) == 64);
+  assert(sizeof(intmax_t) == 8);
 
   uintmax_t n;
   int err = compact_decode_uint(state, result ? &n : NULL);
