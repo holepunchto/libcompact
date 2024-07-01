26b5d6313ec06645f25c546f92bad1b4c5db544e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-07-01 13:50:32 +0200

    Fix test case

--- test/array-utf8.c
+++ test/array-utf8.c
@@ -7,37 +7,37 @@
 
 int
 on_preencode (compact_state_t *state, void *array, size_t i, void *data) {
-  utf8_t **strings = (utf8_t **) array;
+  utf8_string_view_t *strings = (utf8_string_view_t *) array;
 
-  return compact_preencode_utf8(state, strings[i], strlen((const char *) strings[i]));
+  return compact_preencode_utf8(state, strings[i]);
 }
 
 int
 on_encode (compact_state_t *state, void *array, size_t i, void *data) {
-  utf8_t **strings = (utf8_t **) array;
+  utf8_string_view_t *strings = (utf8_string_view_t *) array;
 
-  return compact_encode_utf8(state, strings[i], strlen((const char *) strings[i]));
+  return compact_encode_utf8(state, strings[i]);
 }
 
 int
 on_decode (compact_state_t *state, void *array, size_t i, void *data) {
-  utf8_t **strings = (utf8_t **) array;
+  utf8_string_view_t *strings = (utf8_string_view_t *) array;
 
-  return compact_decode_utf8(state, &strings[i], NULL);
+  return compact_decode_utf8(state, &strings[i]);
 }
 
 void *
 on_alloc (size_t len, void *data) {
-  return malloc(len * sizeof(utf8_t *));
+  return malloc(len * sizeof(utf8_string_view_t));
 }
 
 int
 main () {
   int err;
 
-  utf8_t *array[] = {
-    (utf8_t *) "hello",
-    (utf8_t *) "world",
+  utf8_string_view_t array[] = {
+    utf8_string_view_init((utf8_t *) "hello", 5),
+    utf8_string_view_init((utf8_t *) "world", 5),
   };
 
   compact_state_t state = {0, 0};
@@ -52,13 +52,13 @@ main () {
 
   state.start = 0;
 
-  utf8_t **decoded;
+  utf8_string_view_t *decoded;
   size_t len;
   err = compact_decode_array(&state, (void **) &decoded, &len, NULL, on_alloc, on_decode);
   assert(err == 0);
   assert(len == 2);
 
   for (size_t i = 0; i < len; i++) {
-    assert(strcmp((char *) array[i], (char *) decoded[i]) == 0);
+    assert(utf8_string_view_compare(array[i], decoded[i]) == 0);
   }
 }
